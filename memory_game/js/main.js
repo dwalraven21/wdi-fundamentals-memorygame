150b382948a7161803f8b8e0abedad33801aa06c
Dani Walraven <dwalraven21@gmail.com>
2019-05-08 14:39:52 -0400

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,15 @@
+// Make sure JavaScript is running.
+console.log("Up and running!");
+
+// Create strings for cards 1-4, king or queen.
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree= "king";
+var cardFour= "king";
+
+/* Display the value of two cards, 
+which will represent the cards 
+that the user has flipped over. */
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
