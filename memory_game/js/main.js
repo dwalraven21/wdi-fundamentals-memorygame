5b4594e9baa7665d9f568048be756ce9c07bd00f
Dani Walraven <dwalraven21@gmail.com>
2019-05-15 19:37:30 -0400

    Created game logic.

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -4,7 +4,28 @@ console.log("Up and running!");
 //Create an array cards.
 //The array should hold four strings - one for each of our cards.
 //The strings should be "queen", "queen", "king", and "king".
-const cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 
 //Create an array cardsInPlay.
 //Set its value to an empty array []
@@ -27,12 +48,12 @@ function flipCard(cardID) {
 	function is called to access this card from the cards array 
 	like so: cards[cardId].The message that is logged in the console 
 	should read "User flipped queen" or "User flipped king". */
-	console.log("User flipped " + cards[cardID] + ".")
+	console.log("User flipped " + cards[cardID].rank + " of " + cards[cardID].suit + ". Image: " + cards[cardID].cardImage) 
 
 
 	//Use the push method to add the card that the user flipped 
 	//(cards[cardId]) to the cardsInPlay array.
-	cardsInPlay.push(cards[cardID]);
+	cardsInPlay.push(cards[cardID].rank);
 
 	//Call checkForMatch function.
 	if (cardsInPlay.length === 2) {
@@ -45,4 +66,6 @@ flipCard(0);
 
 //Call the flipCard function again, passing in 2 as an argument.
 flipCard(2);
+
+console.log()
 
